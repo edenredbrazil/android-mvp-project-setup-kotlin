fbb8be9c66a3cc95d0ba40fb4484e39711048fdd
rodrigosimoesrosa <rodrigosimoesrosa@gmail.com>
2017-11-01 11:24:46 -0200

    Files removed

--- app/src/main/java/br/com/edenred/kotlinmvpprojectbase/model/A.kt
+++
@@ -1,8 +0,0 @@
-package br.com.edenred.kotlinmvpprojectbase.model
-
-import java.io.Serializable
-
-/**
- * Created by rodrigosimoesrosa on 5/29/17.
- */
-data class A(val name:String, val age:Int): Serializable
